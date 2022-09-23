683720ccbd6756beb81c094b6fb696a3d6491a39
dphuocle <109648895+dphuocle@users.noreply.github.com>
2022-09-23 11:00:02 +0200

    Add files via upload

--- app.c
+++ app.c
@@ -1,17 +1,14 @@
-#include "symbol.h"
+#include "suite.h"
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char **argv)
 {
-   int n = argc > 1 ? atoi(argv[1]) : 0;
-   char **ns;
-   printf("la suite de %d symbole(s): \n", n);
-   while (n-->0)
-   {
-      ns = symbol_new_symbol();
-      printf("%s\n", ns);
-   }
-   free(ns);
-   printf("\n");
-   return EXIT_SUCCESS;
-}
+    int n = argc > 1 ? atoi(argv[1]) : 0;
+    
+    printf("des suites de symbole(s): \n");
+    while (n-- > 0)
+        suite_e_suite(n);
+        
+    printf("\n");
+    return EXIT_SUCCESS;
+}        
--- generator.c
+++ generator.c
@@ -1,8 +1,20 @@
 #include "generator.h"
-#define INITIAL_VALUE 0
-int generator_generate(void)
+#define INITIAL_VALUE 1
+#include <stdio.h>
+int generator_generate(message m)
 {
-   //val est une v.l de classe statique
-   static int val = INITIAL_VALUE;
-   return val++;
-}   
+ 
+    static int val = INITIAL_VALUE;
+    switch (m)
+    {
+    case Debut:
+        val = INITIAL_VALUE;
+        return 0;
+    case Suivant:
+        return val++;
+    default:
+        printf("problemo");
+        break;
+    }
+    return 0;
+}                
